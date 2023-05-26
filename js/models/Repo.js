99105d454a1677d0c971614cfd148bc69ec39516
Rogério Soares <rgrsoares@yahoo.com.br>
2023-05-26 18:00:05 -0300

    Create a module Repo.js

---
+++ js/models/Repo.js
@@ -0,0 +1,17 @@
+class Repo {
+    constructor(repoUrl){
+        this.repoUrl = repoUrl
+    }
+
+    async getRepositories(){
+        try {
+            const response = await fetch(this.repoUrl)
+            this.repos = await response.json()
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+export { Repo }
