e256c2b5b5a0837cb6dd5e426e11bdf0d59b6c57
Rogério Soares <rgrsoares@yahoo.com.br>
2023-05-26 18:21:42 -0300

    Alter base.js file and create userView.js

--- js/views/userView.js
+++ js/views/userView.js
@@ -1,9 +1,23 @@
-export const elements = {
-    profile: document.querySelector(".profile"),
-    input: document.querySelector("input"),
-    repos: document.querySelector(".repo-list")
-}
+import { elements } from "./base.js"
 
-export const clearUI = () => {
-    elements.profile.innerHTML = ""
+export const renderUser = ({
+    avatar_url,
+    html_url,
+    public_repos,
+    followers,
+    following
+}) => {
+    const markup = `
+    <div class="profile-header">
+        <img src="${avatar_url}" alt="Avatar">
+        <a href="${html_url}" target="_blank">Visitar perfil</a>
+    </div>
+    <ul class="profile-list">
+        <li>Repositórios: ${public_repos}</li>
+        <li>Seguidores: ${followers}</li>
+        <li>Seguindo: ${following}</li>
+    </ul>
+    `;
+
+    elements.profile.insertAdjacentHTML('afterbegin', markup)
 }
