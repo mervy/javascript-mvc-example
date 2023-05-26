f213f097671b64e0335c2929dd52766d06e6e845
Rogério Soares <rgrsoares@yahoo.com.br>
2023-05-26 18:13:37 -0300

    Create a repoView.js

---
+++ js/views/repoView.js
@@ -0,0 +1,15 @@
+import { elements } from "./base.js"
+
+export const renderRepositories = (repositories) => {
+    let markup = ""
+
+    repositories.forEach(( {html_url, name}) => {
+        markup += `
+            <a href="${html_url}" class="repo_url" target="_blank">
+            ${name}
+            </a>
+        `
+    });
+
+    elements.repos.innerHTML = markup
+}
